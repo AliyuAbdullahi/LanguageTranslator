a72e66277c1e4d6c202fbe9a970db25ed7af9bf4
Aliyu Abdullahi <aliyuabdullahi@Aliyus-MacBook-Pro.local>
2016-11-22 10:46:18 +0100

    setup basic ui and basic ui test

--- app/src/androidTest/java/com/carisma/languagetranslator/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/carisma/languagetranslator/MainActivityInstrumentationTest.java
@@ -2,12 +2,15 @@ package com.carisma.languagetranslator;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.Espresso.onView;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -16,7 +19,15 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentationTest {
     @Rule
-    ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
+    @Test
+    public void allViewsAvailable() {
+        onView(withId(R.id.spinner_result)).check(matches(isDisplayed()));
+        onView(withId(R.id.spinner_source)).check(matches(isDisplayed()));
+        onView(withId(R.id.word_input)).check(matches(isDisplayed()));
+        onView(withId(R.id.toggle_language)).check(matches(isDisplayed()));
+        onView(withId(R.id.convert_language)).check(matches(isDisplayed()));
+    }
 
 }
